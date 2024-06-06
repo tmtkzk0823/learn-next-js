23ca762e1c3a4c2213f33013335d6f6efd81eaf7
tomita_kazuki <tmtkzk@icloud.com>
2024-06-06 10:13:40 +0900

    done chapter9

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -2,8 +2,8 @@ import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
-import { LatestInvoice } from '@/app/lib/definitions';
 import { fetchLatestInvoices } from '@/app/lib/data';
+
 export default async function LatestInvoices() {
   const latestInvoices = await fetchLatestInvoices();
   return (
