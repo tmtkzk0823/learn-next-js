2a1e229b8f87ea20a3268130e5bdda11cc73bd44
tomita_kazuki <tmtkzk@icloud.com>
2024-06-27 10:12:20 +0900

    検索処理実装

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,9 +10,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   function handleSearch(term: string) {
+    console.log(term);
     const params = new URLSearchParams(searchParams);
     if (term) {
-      params.set('search', term);
+      params.set('query', term);
     } else {
       params.delete('query');
     }
@@ -28,6 +29,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={ searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
